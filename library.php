b9e88ecc69f6a06786e355b0b8f1239abd126ea2
Andrew Cavallaro <andrew.cavallarojr@gmail.com>
2020-11-09 17:28:51 -0500

    Update library.php

--- library.php
+++ library.php
@@ -1,163 +1,179 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-        <link rel="stylesheet" href="./core.css">
+    <link rel="stylesheet" href="./core.css">
 
-        <link rel="stylesheet" href="./library.css">
-    </head>
-    <body>
-    <header>
-            
-            <div class="navbar navbar-dark bg-dark box-shadow">
-              <div class="container d-flex justify-content-between">
-                <a href="./book.php" class="navbar-brand d-flex align-items-center">
-                  <strong>PDF Library</strong>
-                </a>
-              </div>
-            </div>
-          </header>
-            <div class="album py-5 bg-light">
-              <div class="container">
-                <div class="row">
-                  <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                      <img class="card-img-top" src="./images/1.PNG">
-                      <div class="card-body">
+    <link rel="stylesheet" href="./library.css">
+    <title>PDF Library</title>
+</head>
+<body>
+<header>
+
+    <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+            <a href="./library.php" class="navbar-brand d-flex align-items-center">
+                <strong>PDF Library</strong>
+            </a>
+        </div>
+    </div>
+</header>
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="./images/1.PNG" alt="Data Structures and Algorithms">
+                    <div class="card-body">
                         <p class="card-text">ADTs, Data Structures, and Problem Solving with C++ | 2nd Edition</p>
                         <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                          <a href="./books/ADTs, Data Structures, and Problem Solving with C++.pdf"><button type="button" class="btn btn-sm btn-outline-secondary">Read</button></a>
-                          </div>
+                            <div class="btn-group">
+                                <a href="./books/ADTs, Data Structures, and Problem Solving with C++.pdf">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Read</button>
+                                </a>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                      <img class="card-img-top" src="./images/2.PNG">
-                      <div class="card-body">
-                        <p class="card-text">Designing the User Interface: Strategies for Effective Human-Computer Interaction | 6th Edition</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="./images/2.PNG" alt="Human Computer Interaction">
+                    <div class="card-body">
+                        <p class="card-text">Designing the User Interface: Strategies for Effective Human-Computer
+                            Interaction | 6th Edition</p>
                         <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                          <a href="./books/Designing the User Interface - Strategies for Effective Human-Computer Interaction.pdf"><button type="button" class="btn btn-sm btn-outline-secondary">Read</button></a>
-                          </div>
+                            <div class="btn-group">
+                                <a href="./books/Designing the User Interface - Strategies for Effective Human-Computer Interaction.pdf">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Read</button>
+                                </a>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                      <img class="card-img-top" src="./images/3.PNG">
-                      <div class="card-body">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="./images/3.PNG" alt="Operating System Concepts">
+                    <div class="card-body">
                         <p class="card-text">Operating System Concepts | 9th Edition</p>
                         <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                          <a href="./books/Operating System Concepts 9th Edition.pdf"><button type="button" class="btn btn-sm btn-outline-secondary">Read</button></a>
-                          </div>
+                            <div class="btn-group">
+                                <a href="./books/Operating System Concepts 9th Edition.pdf">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Read</button>
+                                </a>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                      <img class="card-img-top" src="./images/4.PNG">
-                      <div class="card-body">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="./images/4.PNG" alt="Software Design">
+                    <div class="card-body">
                         <p class="card-text">Software Engineering Design | Theory and Practice</p>
                         <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                          <a href="./books/Software_Engineering_Design_Theory_and_Practice.pdf"><button type="button" class="btn btn-sm btn-outline-secondary">Read</button></a>
-                          </div>
+                            <div class="btn-group">
+                                <a href="./books/Software_Engineering_Design_Theory_and_Practice.pdf">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Read</button>
+                                </a>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                      <img class="card-img-top" src="./images/5.PNG">
-                      <div class="card-body">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="./images/5.PNG" alt="29148">
+                    <div class="card-body">
                         <p class="card-text">IEEE | 29418: Software Engineering Requirements</p>
                         <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                          <a href="./books/29148.pdf"><button type="button" class="btn btn-sm btn-outline-secondary">Read</button></a>
-                          </div>
+                            <div class="btn-group">
+                                <a href="./books/29148.pdf">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Read</button>
+                                </a>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                      <img class="card-img-top" src="./images/6.PNG">
-                      <div class="card-body">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="./images/6.PNG" alt="IEEE Glossary">
+                    <div class="card-body">
                         <p class="card-text">IEEE | Standard Glossary of Software Engineering Terminology</p>
                         <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                          <a href="./books/IEEE_SoftwareEngGlossary.pdf"><button type="button" class="btn btn-sm btn-outline-secondary">Read</button></a>
-                          </div>
+                            <div class="btn-group">
+                                <a href="./books/IEEE_SoftwareEngGlossary.pdf">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Read</button>
+                                </a>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                      <img class="card-img-top" src="./images/7.PNG">
-                      <div class="card-body">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="./images/7.PNG" alt="Project Scope">
+                    <div class="card-body">
                         <p class="card-text">Four Techniques for Defining Project Scope</p>
                         <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                          <a href="./books/Project Scope.pdf"><button type="button" class="btn btn-sm btn-outline-secondary">Read</button></a>
-                          </div>
+                            <div class="btn-group">
+                                <a href="./books/Project Scope.pdf">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Read</button>
+                                </a>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                      <img class="card-img-top" src="./images/8.PNG">
-                      <div class="card-body">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="./images/8.PNG" alt="MITRE">
+                    <div class="card-body">
                         <p class="card-text">MITRE | Software Engineering Guide</p>
                         <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                          <a href="./books/Software Engineering Guide.pdf"><button type="button" class="btn btn-sm btn-outline-secondary">Read</button></a>
-                          </div>
+                            <div class="btn-group">
+                                <a href="./books/Software Engineering Guide.pdf">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Read</button>
+                                </a>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                      <img class="card-img-top" src="./images/9.PNG">
-                      <div class="card-body">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="./images/9.PNG" alt="SWEBOK">
+                    <div class="card-body">
                         <p class="card-text">Software Engineering Book of Knowledge | Version 3.0</p>
                         <div class="d-flex justify-content-between align-items-center">
-                          <div class="btn-group">
-                          <a href="./books/SWEBOK.pdf"><button type="button" class="btn btn-sm btn-outline-secondary">Read</button></a>
-                          </div>
+                            <div class="btn-group">
+                                <a href="./books/SWEBOK.pdf">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Read</button>
+                                </a>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
                 </div>
-              </div>
             </div>
-      
-          <footer class="text-muted">
-            <div class="container">
-              <p class="float-right">
-                <a href="#">Back to top</a>
-              </p>
-              <p style="text-align: center;">PDF Library &copy; 2020</p>
-            </div>
-          </footer>
-        
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    </body>
+        </div>
+    </div>
+</div>
+
+<footer class="text-muted">
+    <div class="container">
+        <p class="float-right">
+            <a href="#">Back to top</a>
+        </p>
+        <p style="text-align: center;">PDF Library &copy; 2020</p>
+    </div>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
 </html>
